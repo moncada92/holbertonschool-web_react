bc1d9bfde9f2cd2b921f19753d9f73b9d663044d
Camilo Moncada <camilo.moncada1992@gmail.com>
2021-06-25 07:41:36 -0500

    fixed 2 task1

--- 0x01-Webpack/task_1/webpack.config.js
+++ 0x01-Webpack/task_1/webpack.config.js
@@ -1,8 +1,8 @@
 const path = require("path");
 
 module.exports = {
-  entry: './js/dashboard_main.js',
-  mode: 'production',
+  entry: "./js/dashboard_main.js",
+  mode: "production",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public")
